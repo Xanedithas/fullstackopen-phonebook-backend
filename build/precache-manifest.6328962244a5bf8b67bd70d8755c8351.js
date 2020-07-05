908059a74a67402f02351140949b738f13e4d72a
Xanedithas <mikestaadegaard@hotmail.com>
2020-07-05 12:11:20 +0200

    uibuild

---
+++ build/precache-manifest.6328962244a5bf8b67bd70d8755c8351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fec24a95729931c0479171662c1ecf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64a2dc725ee254b1dd8e",
+    "url": "/static/css/main.8c53bfad.chunk.css"
+  },
+  {
+    "revision": "ab02f96b948239b78bf2",
+    "url": "/static/js/2.0d214d39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d214d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a2dc725ee254b1dd8e",
+    "url": "/static/js/main.68ab8dd6.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
